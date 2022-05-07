04a6a1977db700dd1a757e2ce12ce26dd9f77a0b
Coby7474 <94398327+Coby7474@users.noreply.github.com>
2022-05-06 23:56:06 -0400

    Update code.js

--- code.js
+++ code.js
@@ -1,5 +1,5 @@
 document.onload = function(){
-var win = document..getElementById("emb")
+var win = document.getElementById("emb")
 if (win) {
 win.src = "https://bxbzshxcksourze000765432345678.github.io/projects/" + win.name
 }
