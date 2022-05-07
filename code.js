49b2fe1cc570bbcfda630cf4ac6b8935dd5d4bd4
Coby7474 <94398327+Coby7474@users.noreply.github.com>
2022-05-06 23:52:11 -0400

    Update code.js

--- code.js
+++ code.js
@@ -1,4 +1,4 @@
-window.onload = function(){
+document.onload = function(){
 var win = document.getElementFromId("emb")
 if (win) {
 win.src = "https://bxbzshxcksourze000765432345678.github.io/projects/" + win.name
